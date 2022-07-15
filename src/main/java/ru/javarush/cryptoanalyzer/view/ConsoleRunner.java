b64463655e65101c39271e84800197f08360783e
lzhelisichka <kseniya.rudenko@icloud.com>
2022-07-15 10:39:39 +0300

    draft view

---
+++ src/main/java/ru/javarush/cryptoanalyzer/view/ConsoleRunner.java
@@ -0,0 +1,18 @@
+package ru.javarush.cryptoanalyzer.view;
+import ru.javarush.cryptoanalyzer.kislyakova.controller.MainController;
+
+import java.awt.*;
+import java.util.Scanner;
+
+public class ConsoleRunner {
+    //эт набросок
+//    public static void main(String[] args) {
+//        //build console app
+//        Scanner scan = new Scanner(System.in);
+//        Menu menu = new Menu(scan);
+//        MainController mainController = new MainController();
+//        ConsoleApp app = new ConsoleApp(mainController);
+//        //run console app
+//        app.run(args);
+//    }
+}
